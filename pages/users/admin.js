ba574af3259b44c617de34e77e970427b970bd52
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2021-11-17 18:19:26 +0600

    first commit

---
+++ pages/users/admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const admin = () => {
+    return (
+        <div>
+            <h2>This is admins under users</h2>
+        </div>
+    );
+};
+
+export default admin;
